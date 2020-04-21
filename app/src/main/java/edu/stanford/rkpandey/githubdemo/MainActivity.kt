da6e83f978e3077cfd0709df1a8897a399d901b7
Rahul Pandey <hello@rkpandey.com>
2020-04-20 18:23:23 -0700

    Added button to change text

--- app/src/main/java/edu/stanford/rkpandey/githubdemo/MainActivity.kt
+++ app/src/main/java/edu/stanford/rkpandey/githubdemo/MainActivity.kt
@@ -2,11 +2,17 @@ package edu.stanford.rkpandey.githubdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnChangeText.setOnClickListener {
+            tvHello.text = "Github demo!!"
+        }
+
     }
 }
